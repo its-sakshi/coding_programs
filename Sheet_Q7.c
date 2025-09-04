d5c26e8d9b33807a6186026d46da3f4b55e1b2c2
its-sakshi <77jainsakshi@gmail.com>
2025-09-04 16:54:22 +0530

    primenum

---
+++ Sheet_Q7.c
@@ -0,0 +1,28 @@
+//C program to check whether a number is prime using a for loop:
+
+#include <stdio.h>
+
+int main() {
+    int num, i, isPrime = 1;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num <= 1) {
+        isPrime = 0;  
+    } else {
+        for (i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+
+  if (isPrime == 1) {
+        printf("%d is a Prime number.\n", num);
+    } else {
+        printf("%d is NOT a Prime number.\n", num);
+    }
+
+    return 0;
+}
