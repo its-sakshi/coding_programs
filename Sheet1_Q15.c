9bf816e3880b2fc5ba94a95e947b15bf0b088d69
its-sakshi <77jainsakshi@gmail.com>
2025-09-04 17:09:19 +0530

    GCD

---
+++ Sheet1_Q15.c
@@ -0,0 +1,23 @@
+// C program to find the Greatest Common Divisor (GCD) of two numbers using a while loop
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    while (a != b) {
+        if (a > b) {
+            a = a - b;
+        } 
+        else {
+            b = b - a;
+        }
+    }
+
+    printf("GCD = %d\n", a);
+
+    return 0;
+}
