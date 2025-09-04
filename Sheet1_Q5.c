f669d0ec343e945f26b560bb9fd3f55e504bf75d
its-sakshi <77jainsakshi@gmail.com>
2025-09-04 16:48:00 +0530

    Update Sheet1_Q5.c

--- Sheet1_Q5.c
+++ Sheet1_Q5.c
@@ -1,17 +1,16 @@
-// C program to print the first 100 natural numbers using a while loop
+// C program to print the sum of first 100 natural numbers using a while loop
 
 #include <stdio.h>
 
 int main() {
-    int i = 1;  // starting number
+    int i = 1, sum =0;  
 
     printf("The first 100 natural numbers are:\n");
-
-    // while loop
     while (i <= 100) {
-        printf("%d\n", i);
+        sum += i;
         i++;
     }
+            printf("%d\n", sum);
 
     return 0;
 }
