cfe942a2acc85aa18e89b12311b6555b3f73d1c9
its-sakshi <77jainsakshi@gmail.com>
2025-09-04 16:44:22 +0530

    10naturalnum

---
+++ Sheet1_Q4.c
@@ -0,0 +1,16 @@
+// c program to print first 10 natural numbers using for loop
+
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    // Print first 10 natural numbers
+    printf("The first 10 natural numbers are:\n");
+    for (i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
+
