9193f856807e90f7ace8aec5b485b52422182a13
its-sakshi <77jainsakshi@gmail.com>
2025-09-05 10:38:28 +0530

    table

---
+++ Sheet_Q2.c
@@ -0,0 +1,11 @@
+//a program to print the multiplication table of a number using a for loop
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter a number :");
+    scanf("%d", &n);
+    for(int i=1; i<=10; i++){
+        printf(" %d * %d = %d \n", n,i, n*i);
+    }
+    return 0;
+}
