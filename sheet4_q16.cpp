db842b69f52ccfaa155a5cde74f2e7a22bce181d
its-sakshi <77jainsakshi@gmail.com>
2025-09-30 18:20:43 +0530

    Create sheet4_q16.cpp

---
+++ sheet4_q16.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+void insert(Node*& head, int val) {
+    Node* newNode = new Node(val);
+    if (!head) {
+        head = newNode;
+        return;
+    }
+    Node* temp = head;
+    while (temp->next)
+        temp = temp->next;
+    temp->next = newNode;
+}
+
+bool detectCycle(Node* head) {
+    Node* slow = head;
+    Node* fast = head;
+
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) return true;
+    }
+    return false;
+}
+
+int main() {
+    Node* head = nullptr;
+    insert(head, 1);
+    insert(head, 2);
+    insert(head, 3);
+    insert(head, 4);
+    insert(head, 5);
+
+    // Create a cycle manually (last node pointing to node with value 3)
+    head->next->next->next->next->next = head->next->next;
+
+    if (detectCycle(head))
+        cout << "Cycle detected" << endl;
+    else
+        cout << "No cycle" << endl;
+
+    return 0;
+}
